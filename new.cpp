82b2dba393be745a53acc32c1cfccb341679a3c6
SystemCallBeam <90573740+SystemCallBeam@users.noreply.github.com>
2021-09-13 02:08:42 +0700

    Update new.cpp

--- new.cpp
+++ new.cpp
@@ -3,6 +3,12 @@ using namespace std;
 
 int main(){
     char c;
+    int n;
+    cin >> n;
+    for(int i = 0;i < n;i++){
+        cin >> c;
+        cout << c;
+    }
     cin >> c;
     cout << c << endl;
     return 0;
