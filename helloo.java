29ba0cd48c92e4119104cef8c42da2580ecad94b
SystemCallBeam <90573740+SystemCallBeam@users.noreply.github.com>
2021-09-13 02:04:37 +0700

    Add files via upload

---
+++ helloo.java
@@ -0,0 +1,21 @@
+class helloo{
+    public static void main(String[] args) {
+       
+        int bruh = 100;
+        double IN = bruh;
+        System.out.println("hell");
+        System.out.print("hell ");
+        System.out.println(bruh);
+        System.out.println(IN);
+ 
+/* 
+        String a="100", b = "200";
+        double c = Double.parseDouble(a);
+        System.out.println(c);
+        int num = 10;
+        String age = String.valueOf(num);
+        System.out.println("age = "+age);
+ */
+    }
+
+}
